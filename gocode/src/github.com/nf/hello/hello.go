4496ac14da7ef3bbbce32baad0eff64636be8d04
Abhinav Prakash <abv.prakash@uber.com>
2019-01-11 15:41:49 +0530

    add package and test

--- gocode/src/github.com/nf/hello/hello.go
+++ gocode/src/github.com/nf/hello/hello.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-
+import (
+        "fmt"
+        "github.com/nf/string"
+)
 func main() { 
-  fmt.Println("Hello, new gpher!!")
+  fmt.Println(string.Reverse("Hello, new gopher!!"))
 }
